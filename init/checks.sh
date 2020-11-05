68f183e8ca574ad8e0d3206e53fc71372e59b063
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-05 15:21:34 +0100

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -155,9 +155,9 @@ _checkUpstreamRepo() {
 }
 
 _checkUnoffPlugins() {
-    editLastMessage "Checking USERGE-X [Extra] Plugins ..."
+    editLastMessage "💬 Checking DAMIEN-X [Extra] Plugins ..."
     if test $LOAD_UNOFFICIAL_PLUGINS = true; then
-        editLastMessage "\tLoading USERGE-X [Extra] Plugins ..."
+        editLastMessage "\tLoading DAMIEN-X [Extra] Plugins ..."
         replyLastMessage "\t\tClonning ..."
         gitClone --depth=1 https://github.com/code-rgb/Userge-Plugins.git
         editLastMessage "\t\tUpgrading PIP ..."
@@ -170,9 +170,9 @@ _checkUnoffPlugins() {
         cp -r Userge-Plugins/resources/* resources/
         rm -rf Userge-Plugins/
         deleteLastMessage
-        editLastMessage "\tUSERGE-X [Extra] Plugins Loaded Successfully !"
+        editLastMessage "\tDAMIEN-X [Extra] Plugins Loaded Successfully !"
     else
-        editLastMessage "\tUSERGE-X [Extra] Plugins Disabled !"
+        editLastMessage "\tDAMIEN-X [Extra] Plugins Disabled !"
     fi
     deleteLastMessage
 }
