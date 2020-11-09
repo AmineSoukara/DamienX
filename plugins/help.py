409e609a1618a82acd1380c7627a986a2262bffd
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-09 02:47:10 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,15 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['force']))
 def _force(client, message):
-    client.send_message(message.chat.id,
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="markdown",
-        reply_to_message_id=message.message_id
-        )
-
-
-@Client.on_message(filters.private & filters.incoming & filters.command(['forcehelp']))
-def _forcehelp(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
         parse_mode="markdown",
@@ -24,10 +15,10 @@ def _forcehelp(client, message):
         reply_to_message_id = message.message_id
     )
 
-forcehelp_callback_filter = filters.create(lambda _, __, query: query.data.startswith('forcehelp+'))
+force_callback_filter = filters.create(lambda _, __, query: query.data.startswith('force+'))
 
-@Client.on_callback_query(forcehelp_callback_filter)
-def forcehelp_answer(client, callback_query):
+@Client.on_callback_query(force_callback_filter)
+def force_answer(client, callback_query):
     chat_id = callback_query.from_user.id
     message_id = callback_query.message.message_id
     msg = int(callback_query.data.split('+')[1])
@@ -36,24 +27,23 @@ def forcehelp_answer(client, callback_query):
     )
 
 
-
 def map(pos):
     if(pos==1):
         button = [
-            [InlineKeyboardButton(text = '-->', callback_data = "forcehelp+2")]
+            [InlineKeyboardButton(text = '➡️', callback_data = "force+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://sushantgirdhar.github.io"
+        url = "https://t.me/damienhelp"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/ebruiser")],
-            [InlineKeyboardButton(text = 'Owner info', url=url)],
-            [InlineKeyboardButton(text = '<--', callback_data = f"forcehelp+{pos-1}")]
+            [InlineKeyboardButton(text = '🔔 Updates Channel 🔔', url="https://t.me/DamienSoukara")],
+            [InlineKeyboardButton(text = '📣 Support Chat 📣', url=url)],
+            [InlineKeyboardButton(text = '⬅️', callback_data = f"force+{pos-1}")]
         ]
     else:
         button = [
             [
-                InlineKeyboardButton(text = '<--', callback_data = f"forcehelp+{pos-1}"),
-                InlineKeyboardButton(text = '-->', callback_data = f"forcehelp+{pos+1}")
+                InlineKeyboardButton(text = '⬅️', callback_data = f"force+{pos-1}"),
+                InlineKeyboardButton(text = '➡️', callback_data = f"force+{pos+1}")
             ],
         ]
     return button
