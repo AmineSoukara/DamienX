31cbb40b591eae0b8c8793e6840e7031994c2efa
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-11-22 18:27:01 +0100

    Update versions.py

--- userge/versions.py
+++ userge/versions.py
@@ -3,9 +3,9 @@ from sys import version_info
 from pyrogram import __version__ as __pyro_version__  # noqa
 
 __major__ = 0
-__minor__ = 2
-__micro__ = 5
+__minor__ = 3
+__micro__ = 0
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
-__license__ = "[GNU GPL v3.0](https://github.com/AmineSoukara/Damien-X/blob/alpha/LICENSE)"
-__copyright__ = "[DAMIEN-X](https://github.com/AmineSoukara/Damien-X)"
+__license__ = "[GNU GPL v3.0](https://github.com/aminesoukara/damien-x/blob/alpha/LICENSE)"
+__copyright__ = "[DAMIEN-X](https://github.com/aminesoukara/damien-x)"
